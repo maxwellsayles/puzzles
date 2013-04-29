13096d97d6c5ebac31263e55468cdfd28d5ff506
Maxwell Sayles <maxwellsayles@gmail.com>
2013-04-29 13:13:07 -0700

    return type *is* a function, rather than currying, and i golfed it

--- AddIntToString.scala
+++ AddIntToString.scala
@@ -1,17 +1,14 @@
 
 def addIntToString(i: Int, s: String): String = {
-  def last(carry: Int): String = {
-    if (carry == 0) ""
-    else carry.toString
-  }
-  def step(f: Int => String, i: Int)(x: Int): String = {
+  def last = (carry: Int) => if (carry == 0) "" else carry.toString
+  def step(f: Int => String, i: Int) = (x: Int) => {
     val y = (x % 10) + i
     f((y / 10) + (x / 10)) + (y % 10).toString
   }
-  s.toList.map(_.toInt - '0').foldLeft(last _)(step)(i)
+  s.toList.map(_.toInt - '0').foldLeft(last)(step)(i)
 }
 
 println(addIntToString(1, "1999999999999999999999999999999"))
-println(addIntToString(3, "0000000000000000009999999999999"))
+println(addIntToString(2, "0000000000000000009999999999999"))
 println(addIntToString(3, "0000000000000000000000000000000"))
 println(addIntToString(123456789, "0"))
