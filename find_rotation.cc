cf1f696612f47af6ba94ae45bab3af70312ab6ac
Maxwell Sayles <maxwellsayles@gmail.com>
2013-09-05 18:28:43 -0700

    emacs formatting and const int m

--- find_rotation.cc
+++ find_rotation.cc
@@ -16,25 +16,25 @@
 using namespace std;
 
 int find_rotation(const vector<int>& A, const int i, const int j) {
-	if (i >= j) return 0;
-	if (i == j - 1) return A[i] < A[j] ? 0 : j;
-	int m = (i + j) / 2;
-	return A[i] < A[m] ? find_rotation(A, m, j) : find_rotation(A, i, m); 
+  if (i >= j) return 0;
+  if (i == j - 1) return A[i] < A[j] ? 0 : j;
+  const int m = (i + j) / 2;
+  return A[i] < A[m] ? find_rotation(A, m, j) : find_rotation(A, i, m); 
 }
 
 int main(int argc, char** argv) {
-	const int N = 1000;
-	cout << "Testing all vectors 1..n for n=1 through " << N - 1 << endl;
-	cout << "on every possible rotation for each vector." << endl;
-	cout << "This may take a while." << endl;
-	for (int n = 1; n < N; n++) {
-		vector<int> A(n);
-		for (int j = 0; j < n; j++) {  // rotation
-			for (int i = 0; i < n; i++) {  // index
-				A[i] = (i + j) % n;
-			}
-			assert(find_rotation(A, 0, n-1) == (n - j) % n);
-		}
-	}
-	return 0;
+  const int N = 1000;
+  cout << "Testing all vectors 1..n for n=1 through " << N - 1 << endl;
+  cout << "on every possible rotation for each vector." << endl;
+  cout << "This may take a while." << endl;
+  for (int n = 1; n < N; n++) {
+    vector<int> A(n);
+    for (int j = 0; j < n; j++) {  // rotation
+      for (int i = 0; i < n; i++) {  // index
+	A[i] = (i + j) % n;
+      }
+      assert(find_rotation(A, 0, n-1) == (n - j) % n);
+    }
+  }
+  return 0;
 }
