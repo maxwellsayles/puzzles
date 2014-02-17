e0806bf8c29c8ad09be7ddf0d2a8aaf64e846610
maxwellsayles <maxwellsayles@gmail.com>
2014-02-17 13:01:42 -0800

    Should be O(n^3) on a good day ;)

--- LongestSubstringPalindrome.pl
+++ LongestSubstringPalindrome.pl
@@ -1,5 +1,5 @@
 % This is a solution to the longest substring palindrome in Prolog.
-% It is at least \Omega(n^3) in runtime, and possibly O(n^2) on a good day.
+% It is at least \Omega(n^3) in runtime, and possibly O(n^3) on a good day.
 % I have an O(n) solution in LongestSubstringPalindrome.hs, but
 % this was more to learn some prolog and see how easy it would be to write
 % the brute force solution.
