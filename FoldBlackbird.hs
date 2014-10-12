9f9cad7ad63d111775606a652e53a0a1887ea871
Maxwell Sayles <maxwellsayles@gmail.com>
2014-10-11 19:23:34 -0700

    Fold with continues takes a left associative O(n^2) into a right associative O(n).

---
+++ FoldBlackbird.hs
@@ -0,0 +1,28 @@
+{- |
+
+Continuations let you turn left associative operations into right
+associative operations.
+-}
+
+import Data.List
+import System.CPUTime
+
+rightAssoc :: (a -> b -> c) -> (c -> d) -> a -> b -> d
+rightAssoc f = \k x y -> k $ f x y
+
+-- Or by lambdabot, we can write `rightAssoc` pointfree as...                         
+-- rightAssoc = flip ((.) . (.))
+-- Using Data.Aviary.Birds this is
+-- rightAssoc = flip blackbird
+
+timeit x = do
+  start <- getCPUTime
+  return $! x
+  end <- getCPUTime
+  print $ end - start
+
+main = do
+  let n = 10000
+  let xs = map return [1..n]
+  timeit $ length $ foldl' (++) [] xs
+  timeit $ length $ foldl' (rightAssoc (++)) id xs []
