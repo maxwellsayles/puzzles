ff8fde0f8656a03494d1fbe03dbc548e1152bacc
Maxwell Sayles <maxwellsayles@gmail.com>
2015-04-08 19:59:05 -0700

    longest and most anagrams

--- Anagrams.hs
+++ Anagrams.hs
@@ -1,23 +1,43 @@
 {- This one speaks for itself -}
+import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import Data.Char
 import Data.Function
 import Data.List
+import Data.Ord
 import qualified Data.Set as Set
 
-main = mapM_ print .
-       take 10 . 
-       reverse .
-       sortBy (compare `on` (length . head)) .
-       filter ((>1) . length) .
-       map (map fst) .
-       groupBy ((==) `on` snd) .
-       sortBy (compare `on` snd) .
-       map (id &&& sort) .
-       Set.toList .
-       Set.fromList .
-       map (map toLower) .
-       filter (all isLetter) .
-       lines =<<
-       readFile "/etc/dictionaries-common/words"
+longestAnagrams :: [String] -> [[String]]
+longestAnagrams =
+  reverse .
+  sortBy (comparing (length . head)) .
+  filter ((>1) . length) .
+  map (map fst) .
+  groupBy ((==) `on` snd) .
+  sortBy (comparing snd) .
+  map (id &&& sort) .
+  Set.toList . Set.fromList .
+  map (map toLower) .
+  filter (all isLetter)
+
+mostAnagrams :: [String] -> [[String]]
+mostAnagrams =
+  reverse .
+  map (map fst) .
+  sortBy (comparing length) .
+  groupBy ((==) `on` snd) .
+  sortBy (comparing snd) .
+  map (id &&& sort) .
+  Set.toList . Set.fromList .
+  map (map toLower) .
+  filter (all isLetter)
+
+main :: IO ()
+main = do
+  words <- lines <$> readFile "/etc/dictionaries-common/words"
+  putStrLn "10 longest palindromes: "
+  mapM_ print $ take 10 $ longestAnagrams words
+
+  putStrLn "\n10 sets of words with the most anagrams:"
+  mapM_ print $ take 10 $ mostAnagrams words
