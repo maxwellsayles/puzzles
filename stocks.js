43126bc2da83d6f136e08acd80958443a3b8d5d3
Maxwell Sayles <maxwellsayles@gmail.com>
2015-07-16 21:51:51 -0700

    O(1) memory

--- stocks.js
+++ stocks.js
@@ -1,4 +1,14 @@
-var stocks = function (pa, pb) {
+var stocks1 = function (pa, pb) {
+    var a = 0, b = 0, z = 0;
+    for (var i = 1; i < pa.length; i++) {
+	a = Math.max(z, a - pa[i-1] + pa[i]);
+	b = Math.max(z, b - pb[i-1] + pb[i]);
+	z = Math.max(a, b);	
+    }
+    return z;
+}
+
+var stocks2 = function (pa, pb) {
     var opta = [0];
     var optb = [0];
     var opt0 = [0];
@@ -10,5 +20,9 @@ var stocks = function (pa, pb) {
     return opt0[pa.length-1];
 }
 
-console.log(stocks([2,8,1,2],[2,1,5,9]));
+a = [2,8,1,2];
+b = [2,1,5,9];
+console.log(stocks1(a, b));
+console.log(stocks2(a, b))
+
 
