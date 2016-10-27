442a50b2edcf8a9bd91dd644d3ce41f97c807a90
Maxwell Sayles <maxwellsayles@gmail.com>
2016-10-26 21:53:09 -0700

    clean it up and inline the OpNode function

--- PostOrderIter.cs
+++ PostOrderIter.cs
@@ -25,100 +25,94 @@ using System.Collections.Generic;
 
 namespace PostOrderIter
 {
-    class PostOrderIter<T>
+  class PostOrderIter<T>
+  {
+    public PostOrderIter(Tree<T> t)
     {
-        public PostOrderIter(Tree<T> t)
-        {
-            PushNode(t);
-        }
-
-        public bool HasNext()
-        {
-            return ops.Count != 0;
-        }
-
-        public T Next()
-        {
-            return ops.Pop()();
-        }
-
-        delegate T Op();
-        private Stack<Op> ops = new Stack<Op>();
-
-        private T OpNode(Tree<T> t)
-        {
-            PushNode(t);
-            return Next();
-        }
-
-        private void PushNode(Tree<T> t)
-        {
-            if (t != null)
-            {
-                ops.Push(() => t.v);
-                ops.Push(() => OpNode(t.r));
-                ops.Push(() => OpNode(t.l));
-            }
-        }
+      PushNode(t);
+    }
+    
+    public bool HasNext()
+    {
+      return ops.Count != 0;
+    }
+    
+    public T Next()
+    {
+      return ops.Pop()();
     }
 
-    class Tree<T>
+    delegate T Op();
+    private Stack<Op> ops = new Stack<Op>();
+
+    private void PushNode(Tree<T> t)
     {
-        public T v;
-        public Tree<T> l;
-        public Tree<T> r;
-        public Tree(T x)
-        {
-            v = x;
-            l = null;
-            r = null;
-        }
-        public Tree(Tree<T> l, T v, Tree<T> r)
-        {
-            this.v = v;
-            this.l = l;
-            this.r = r;
-        }
+      if (t != null)
+	{
+	  ops.Push(() => t.v);
+	  ops.Push(() => { PushNode(t.r); return Next(); });
+	  ops.Push(() => { PushNode(t.l); return Next(); });
+	}
+    }
+  }
+
+  class Tree<T>
+  {
+    public T v;
+    public Tree<T> l;
+    public Tree<T> r;
+    public Tree(T x)
+    {
+      v = x;
+      l = null;
+      r = null;
+    }
+    public Tree(Tree<T> l, T v, Tree<T> r)
+    {
+      this.v = v;
+      this.l = l;
+      this.r = r;
+    }
+  }
+
+  class Program 
+  {
+    static Tree<int> Tree(int v)
+    {
+      return new Tree<int>(v);
     }
 
-    class Program 
+    static Tree<int> Tree(Tree<int> l, int v, Tree<int> r)
     {
-        static Tree<int> Tree(int v)
-        {
-            return new Tree<int>(v);
-        }
+      return new Tree<int>(l, v, r);
+    }
 
-        static Tree<int> Tree(Tree<int> l, int v, Tree<int> r)
-        {
-            return new Tree<int>(l, v, r);
-        }
+    static Tree<int> Tree(int l, int v, int r)
+    {
+      return Tree(Tree(l), v, Tree(r));
+    }
 
-        static Tree<int> Tree(int l, int v, int r)
-        {
-            return Tree(Tree(l), v, Tree(r));
-        }
-
-        static void DoTree(Tree<int> t) 
-        {
-            var iter = new PostOrderIter<int>(t);
-            while (iter.HasNext())
-            {
-                Console.Write("{0} ", iter.Next());
-            }
-            Console.WriteLine();
-        }
+    static void DoTree(Tree<int> t)
+    {
+      var iter = new PostOrderIter<int>(t);
+      while (iter.HasNext())
+      {
+	Console.Write("{0} ", iter.Next());
+      }
+      Console.WriteLine();
+    }
         
 
-        static void Main(string[] args)
-        {
-            // Print nothing.
-            DoTree(null);
-            // Print 1.
-            DoTree(Tree(1));
-            // Print 1 3 2.
-            DoTree(Tree(1, 2, 3));
-            // Print 1 3 2 5 7 6 4.
-            DoTree(Tree(Tree(1, 2, 3), 4, Tree(5, 6, 7)));
-        }
+    static void Main(string[] args)
+    {
+      // Print nothing.
+      DoTree(null);
+      // Print 1.
+      DoTree(Tree(1));
+      // Print 1 3 2.
+      DoTree(Tree(1, 2, 3));
+      // Print 1 3 2 5 7 6 4.
+      DoTree(Tree(Tree(1, 2, 3), 4, Tree(5, 6, 7)));
     }
+  }
 }
