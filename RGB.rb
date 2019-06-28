407fab23e2339a9414ac8a4dbe9ec36cfed37803
Maxwell Sayles <maxwellsayles@gmail.com>
2019-06-28 08:15:46 -0700

    enumerate RGB permutations. verify by counting

--- RGB.rb
+++ RGB.rb
@@ -26,17 +26,26 @@ def sortRGB(xs)
   xs
 end
 
+def ith(i)
+  return '' if i == 0
+  i -= 1
+  s = ''
+  while i > 2 do
+    s += 'RGB'[i % 3]
+    i = (i - 3) / 3
+  end
+  s += 'RGB'[i % 3]
+  s
+end
 
-puts sortRGB("RGB")
-puts sortRGB("GRB")
-puts sortRGB("GBR")
-puts sortRGB("BGR")
-puts sortRGB("BRG")
-puts sortRGB("RBG")
-puts sortRGB("RRGGBB")
-puts sortRGB("GGRRBB")
-puts sortRGB("GGBBRR")
-puts sortRGB("BBGGRR")
-puts sortRGB("BBRRGG")
-puts sortRGB("RRBBGG")
+def validate(i)
+  s = ith(i)
+  r = s.count('R')
+  g = s.count('G')
+  b = s.count('B')
+  expected = 'R' * r + 'G' * g + 'B' * b
+  fail unless sortRGB(s) == expected
+end
 
+(0..100000).each do |x| validate(x) end
+puts "All tests pass"
