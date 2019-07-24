60210194cc9a167f62aff0983d2a97648ebd7398
Maxwell Sayles <maxwellsayles@gmail.com>
2019-07-23 22:53:54 -0700

    Some command explaining the gibber gabber

--- BiasedToUnbiased.hs
+++ BiasedToUnbiased.hs
@@ -1,3 +1,10 @@
+{-| This simulates an unbiased coin given a biased coin. The protocol is simple.
+Flip the biased coin twice. If it's the same value, start over. Otherwise,
+return the value of the first flip. The intuition behind why this works is that
+when two flips are different, the position of the biased/unbiased value are equally
+likely.
+|-}
+
 import System.Random
 
 biased :: StdGen -> [Bool]
