12a20e5f0b6b3de115ce412e62d6d545bf8ab8c2
Maxwell Sayles <maxwellsayles@gmail.com>
2019-11-06 21:24:35 -0800

    arbitrary tree generation for testing

--- LongestTreePath.hs
+++ LongestTreePath.hs
@@ -11,34 +11,43 @@ Since the height of a tree can be computed in O(n), we can do determine
 the longest path in O(n).
 -}
 
+{-# LANGUAGE FlexibleInstances #-}
+
 import Control.Arrow
 import Control.Monad.State
 import System.Random
+import Test.QuickCheck
 
 data Tree a = Empty | Node a (Tree a) (Tree a)
+
+instance Arbitrary (Tree Int) where
+  arbitrary = do
+    low <- arbitrary
+    high <- sized (return . (low +))
+    randomTree low high
 
 {-
 Generate a random binary search tree that includes each of the
 elements in the range [low, high] inclusive.
 -}
+randomTree :: (Ord a, Random a, Num a) => a -> a -> Gen (Tree a)
 randomTree low high
     | low > high  = return Empty
     | low == high = return $! Node low Empty Empty
-    | otherwise   = do mid <- state $ randomR (low, high)
-                       left <- randomTree low (mid-1)
-                       right <- randomTree (mid+1) high
-                       return $! Node mid left right
+    | otherwise   = do
+        mid <- choose (low, high)
+        left <- randomTree low (mid-1)
+        right <- randomTree (mid+1) high
+        return $! Node mid left right
 
 {- Returns (height, longestPathSoFar) -}
+longestPath :: (Num b, Ord b) => Tree a -> (b, b)
 longestPath Empty = (0, 0)
 longestPath (Node _ l r) =
     let (lh, lx) = longestPath l
         (rh, rx) = longestPath r
-    in  (1 + max lh rh, maximum [lx, rx, lh+rh+2])
+    in  (1 + max lh rh, maximum [lx, rx, lh + rh + 2])
 
-main = print .
-       snd .
-       longestPath .
-       evalState (randomTree (0::Int) 100000) =<<
-       newStdGen
-
+main :: IO ()
+main = do
+  return ()
