3b200f75bfd40d80ea4feb89b396837ab8d4f304
Maxwell Sayles <maxwellsayles@gmail.com>
2024-11-22 14:31:07 -0800

    Additional tests.

--- WaterContainer.rs
+++ WaterContainer.rs
@@ -21,6 +21,10 @@ fn water_container_brute(heights: &Vec<u32>) -> u32 {
 // This works by iterating i and j from the outside in moving the index that
 // points to the lower height.
 fn water_container_greedy(heights: &Vec<u32>) -> u32 {
+    if heights.is_empty() {
+	return 0;
+    }
+
     let mut i = 0;
     let mut j = heights.len() - 1;
     let mut max_area = 0;
@@ -45,12 +49,76 @@ mod tests {
 
     #[test]
     fn test_leetcode_example() {
-	let heights = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+	let heights = vec![1,8,6,2,5,4,8,3,7];
 
 	let area_brute = water_container_brute(&heights);
 	assert_eq!(area_brute, 49);
 
 	let area_greedy = water_container_greedy(&heights);
 	assert_eq!(area_greedy, 49);
+    }
+
+    #[test]
+    fn test_empty() {
+	let area_brute = water_container_brute(&Vec::new());
+	assert_eq!(area_brute, 0);
+
+	let area_greedy = water_container_greedy(&Vec::new());
+	assert_eq!(area_greedy, 0);
+    }
+
+    #[test]
+    fn test_single() {
+	let heights = vec![1];
+
+	let area_brute = water_container_brute(&heights);
+	assert_eq!(area_brute, 0);
+
+	let area_greedy = water_container_greedy(&heights);
+	assert_eq!(area_greedy, 0);
+    }
+
+    #[test]
+    fn test_ascending() {
+	let heights = vec![1,2,3,4,5,6,7,8,9];
+
+	let area_brute = water_container_brute(&heights);
+	assert_eq!(area_brute, 20);
+
+	let area_greedy = water_container_greedy(&heights);
+	assert_eq!(area_greedy, 20);
+    }
+
+    #[test]
+    fn test_descending() {
+	let heights = vec![9,8,7,6,5,4,3,2,1];
+
+	let area_brute = water_container_brute(&heights);
+	assert_eq!(area_brute, 20);
+
+	let area_greedy = water_container_greedy(&heights);
+	assert_eq!(area_greedy, 20);
+    }
+
+    #[test]
+    fn test_v() {
+	let heights = vec![9,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9];
+
+	let area_brute = water_container_brute(&heights);
+	assert_eq!(area_brute, 9 * 16);
+
+	let area_greedy = water_container_greedy(&heights);
+	assert_eq!(area_greedy, 9 * 16);
+    }
+
+    #[test]
+    fn test_inv() {
+	let heights = vec![1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1];
+
+	let area_brute = water_container_brute(&heights);
+	assert_eq!(area_brute, 4 * 10); // or 5 * 8
+
+	let area_greedy = water_container_greedy(&heights);
+	assert_eq!(area_greedy, 4 * 10);
     }
 }
