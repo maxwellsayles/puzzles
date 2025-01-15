adf0afb6f9c7c8fee0586d51afd3c058ffe43e5e
Maxwell Sayles <maxwellsayles@gmail.com>
2025-01-15 10:32:14 -0800

    Updated comments and used j = n - 1 for clarity for the start of the subset sliding window.

--- anagram_indices.py
+++ anagram_indices.py
@@ -3,13 +3,12 @@ The naive method iterates each haystack subset of needle length and compares
 the sorted subset with the sorted needle. This takes O(n*m*logm) time and O(n)
 memory.
 
-The linear method first computes the character frequency of the needle.
-Then iterates two indices, the length of the needle + 1 apart. The smaller
-index is one character before the subset considered. Then or the smaller index,
-the frequency for that character is incremented, and for the larger index, the
-frequency for that character is decremented. For any frequency that is 0, the
-entry is deleted. When the frequency dict is empty, the value of the smaller
-index + 1 is added to the result. This takes O(n) time and O(n) memory.
+The linear method first computes the character frequency of the needle,
+then iterates two indices the length of the needle + 1 apart. This represents
+a sliding window of characters moving into the subset and moving out of the
+subset. The frequency difference of the subset from the needle is updated with
+each iteration. When the frequency difference is zero, the index of the start of
+the subset is added to the result set. This takes O(n) time and O(n) memory.
 """
 
 import unittest
@@ -31,8 +30,9 @@ def anagram_indices_linear(needle, haystack):
 	freq = Counter(needle)
 	res = []
 	for i in range(len(haystack)):
-		if i - n >= 0:
-			c = haystack[i - n]
+		j = i - n
+		if j >= 0:
+			c = haystack[j]
 			freq[c] = (freq.get(c) or 0) + 1
 			if freq[c] == 0:
 				del freq[c]
@@ -43,7 +43,7 @@ def anagram_indices_linear(needle, haystack):
 			del freq[c]
 
 		if len(freq) == 0:
-			res.append(i - n + 1)
+			res.append(j + 1)
 
 	return res
 
