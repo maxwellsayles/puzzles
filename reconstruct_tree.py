74cdc57859f0d0e3b2b76b8c0d40f8bc8676c5d0
Maxwell Sayles <maxwellsayles@gmail.com>
2025-02-10 13:51:58 -0800

    Reconstruct from post-order and in-order traversals.

--- reconstruct_tree.py
+++ reconstruct_tree.py
@@ -25,6 +25,13 @@ def pre_order(node):
 	yield from pre_order(node.left)
 	yield from pre_order(node.right)
 
+def post_order(node):
+	if not node:
+		return
+	yield from post_order(node.left)
+	yield from post_order(node.right)
+	yield node.value
+
 def pre_in_order_tree(pre_order, in_order):
 	if not pre_order:
 		return None
@@ -32,6 +39,15 @@ def pre_in_order_tree(pre_order, in_order):
 	idx = in_order.index(root)
 	left = pre_in_order_tree(pre_order[1:idx+1], in_order[:idx])
 	right = pre_in_order_tree(pre_order[idx+1:], in_order[idx+1:])
+	return Node(root, left, right)
+
+def post_in_order_tree(post_order, in_order):
+	if not post_order:
+		return None
+	root = post_order[-1]
+	idx = in_order.index(root)
+	left = post_in_order_tree(post_order[:idx], in_order[:idx])
+	right = post_in_order_tree(post_order[idx:-1], in_order[idx+1:])
 	return Node(root, left, right)
 
 class TestPreInOrderTree(unittest.TestCase):
@@ -45,9 +61,14 @@ class TestPreInOrderTree(unittest.TestCase):
 		self.assertEqual(node.left, None)
 		self.assertEqual(node.right, None)
 
-	def test_eg1(self):
+	def test_eg1_pre_in_order(self):
 		node = pre_in_order_tree('abdecfg', 'dbeafcg')
 		self.assertEqual(''.join(pre_order(node)), 'abdecfg')
+		self.assertEqual(''.join(in_order(node)), 'dbeafcg')
+
+	def test_eg1_post_in_order(self):
+		node = post_in_order_tree('bdecfga', 'dbeafcg')
+		self.assertEqual(''.join(post_order(node)), 'bdecfga')
 		self.assertEqual(''.join(in_order(node)), 'dbeafcg')
 
 if __name__ == '__main__':
