ade027fd2138c4a6123d7f0452ed1b1b728aa86b
Maxwell Sayles <maxwellsayles@gmail.com>
2025-02-10 13:24:03 -0800

    Reconstruct a binary tree from pre-order and in-order traversals.

---
+++ reconstruct_tree.py
@@ -0,0 +1,54 @@
+"""
+Reconstruct a binary tree given the pre-order and in-order traversal of the
+tree values.
+"""
+
+import unittest
+
+class Node:
+	def __init__(self, value, left, right):
+		self.value = value
+		self.left = left
+		self.right = right
+
+def in_order(node):
+	if not node:
+		return
+	yield from in_order(node.left)
+	yield node.value
+	yield from in_order(node.right)
+
+def pre_order(node):
+	if not node:
+		return
+	yield node.value
+	yield from pre_order(node.left)
+	yield from pre_order(node.right)
+
+def pre_in_order_tree(pre_order, in_order):
+	if not pre_order:
+		return None
+	root = pre_order[0]
+	idx = in_order.index(root)
+	left = pre_in_order_tree(pre_order[1:idx+1], in_order[:idx])
+	right = pre_in_order_tree(pre_order[idx+1:], in_order[idx+1:])
+	return Node(root, left, right)
+
+class TestPreInOrderTree(unittest.TestCase):
+
+	def test_empty(self):
+		self.assertEqual(pre_in_order_tree([], []), None)
+
+	def test_single(self):
+		node = pre_in_order_tree('a', 'a')
+		self.assertEqual(node.value, 'a')
+		self.assertEqual(node.left, None)
+		self.assertEqual(node.right, None)
+
+	def test_eg1(self):
+		node = pre_in_order_tree('abdecfg', 'dbeafcg')
+		self.assertEqual(''.join(pre_order(node)), 'abdecfg')
+		self.assertEqual(''.join(in_order(node)), 'dbeafcg')
+
+if __name__ == '__main__':
+	unittest.main()
